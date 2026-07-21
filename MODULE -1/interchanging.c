40e8cc775b46e37ca9f6aa0025dd0a24ab0528cc
bindu519 <himabindukanakala123@gmail.com>
2026-07-21 10:18:37 +0530

    Create interchanging.c

---
+++ MODULE -1/interchanging.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = 0, min = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > arr[max])
+            max = i;
+        if (arr[i] < arr[min])
+            min = i;
+    }
+
+    int temp = arr[max];
+    arr[max] = arr[min];
+    arr[min] = temp;
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+       
+    }
+
+    return 0;
+}
